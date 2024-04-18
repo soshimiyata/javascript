9755c73d053066229e644c12e3e6cc0a420ed031
soshimiyata <gchuangshi@gmail.com>
2024-04-18 11:49:45 +0900

    new

---
+++ main.js
@@ -0,0 +1,35 @@
+let count = 0;
+count = 30;
+let newCount = 0;
+const daysInWeek = 7;
+
+let additionResult = 2 + 5;
+let result = 5;
+result = result + 1;
+result += 1;
+const userName = `soshi`;
+string = "Hello" + userName;
+string = `Hello ${userName}w`;
+string = "Hello\nsoshoi";
+const userInput = "10";
+let calResult;
+calcResult = Number(userInput) + 10;
+
+let array = ["apple", "banana", "grape"];
+console.log(array[0]);
+const coffee = {
+  name: "chocolateMocha",
+};
+console.log(coffee.name);
+function add(num1, num2) {
+  console.log(num1, num2);
+  return num1 + num2;
+  console.log("hello");
+}
+const returnedValue = add(2, 4);
+console.log(returnedValue);
+// comment
+/*
+this
+is 
+comment*/
