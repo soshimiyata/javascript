788efd052aa876ef45b0c4d7c128e5332ceed4fe
soshimiyata <gchuangshi@gmail.com>
2024-04-24 00:32:57 +0900

    object

--- object.js
+++ object.js
@@ -8,4 +8,47 @@ const person = {
   const: "const",
   [interests]: ["music", "travel"],
 };
+for (const key in person) {
+  console.log(key);
+}
 console.log(person["const"]);
+console.log(Object.keys(person));
+// for (const key of Object.keys(person)) {
+//   console.log(key);
+// }
+console.log(Object.entries(person));
+
+// プロパティ削除
+person.name = "jack";
+person.gender = "man";
+delete person.age;
+console.log(person);
+
+// 省略記法
+const name = "espresso";
+const size = 350;
+const coffee = {
+  name,
+  size,
+};
+console.log(coffee);
+// スプレット構文
+const coffee2 = {
+  ...coffee,
+  ishot: true,
+};
+console.log(coffee2 === coffee);
+
+const o1 = { a: 1 };
+const o2 = { a: 2, b: 2 };
+const o3 = { a: 2, b: 2, c: 3 };
+Object.assign(o1, o2, o3);
+console.log(o1);
+
+const book = {
+  title: "book course",
+  price: 9.99,
+};
+// const title = book.title;
+const { title, price } = book;
+console.log(title, price);
